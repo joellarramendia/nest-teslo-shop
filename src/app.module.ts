82f7e2e5b51952ab9dbacabee40e368089aacfa7
joellarramendia <joellarramendia@gmail.com>
2026-08-21 20:55:53 -0300

    add ProductsModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 
 @Module({
@@ -16,7 +17,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true
-    })
+    }),
+
+    ProductsModule
   ],
 })
 export class AppModule {}
