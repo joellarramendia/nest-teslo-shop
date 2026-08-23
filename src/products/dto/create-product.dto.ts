d9d4b544c908c043dacf30b1438e8c1d575c068c
joellarramendia <joellarramendia@gmail.com>
2026-08-22 22:53:38 -0300

    feat: add tags field to product entity and DTO

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -29,5 +29,10 @@ export class CreateProductDto {
 
     @IsIn(['men', 'women', 'kid', 'unisex'])
     gender: string
+
+    @IsString({each: true})
+    @IsArray()
+    @IsOptional()
+    tags: string[]
 }
 
