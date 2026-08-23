a2016a76383efe3ff9d19d839f789c70f312a4f6
joellarramendia <joellarramendia@gmail.com>
2026-08-22 22:04:58 -0300

    feat: added pagination

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -38,8 +39,13 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return this.productRepository.find({})
+  findAll(paginationDto: PaginationDto) {
+    const {limit = 10, offset = 10} = paginationDto
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+      // TODO: relaciones
+    })
   }
 
   async findOne(id: string) {
