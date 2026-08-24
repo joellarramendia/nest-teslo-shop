682dae024b848ad341e75240c90215b02af71e21
joellarramendia <joellarramendia@gmail.com>
2026-08-24 17:34:06 -0300

    fix: export TypeOrmModule in ProductsModule for improved module accessibility

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -10,6 +10,7 @@ import { ProductImage } from './entities/product-image.entity';
   providers: [ProductsService],
   imports: [
     TypeOrmModule.forFeature([Product, ProductImage])
-  ]
+  ],
+  exports: [ProductsService, TypeOrmModule]
 })
 export class ProductsModule {}
