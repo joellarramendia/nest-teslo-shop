8420fefa0630782f6eec71572da817a5aa1cea45
joellarramendia <joellarramendia@gmail.com>
2026-08-24 15:49:16 -0300

    feat: add ProductImage entity and update Product entity to include images relation

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { Product } from './entities/product.entity';
+import { ProductImage } from './entities/product-image.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   imports: [
-    TypeOrmModule.forFeature([Product])
+    TypeOrmModule.forFeature([Product, ProductImage])
   ]
 })
 export class ProductsModule {}
