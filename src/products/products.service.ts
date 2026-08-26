78aaa1627b772ee555c21cfeb3764ee46f2ae573
joellarramendia <joellarramendia@gmail.com>
2026-08-26 17:01:57 -0300

    feat: include user information in product creation and update methods

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -7,6 +7,7 @@ import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid'
 import { ProductImage } from './entities/product-image.entity';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class ProductsService {
@@ -23,12 +24,13 @@ export class ProductsService {
     private readonly dataSource: DataSource
   ) { }
 
-  async create(createProductDto: CreateProductDto) {
+  async create(createProductDto: CreateProductDto, user: User) {
     try {
       const { images = [], ...productDetails } = createProductDto
       const product = this.productRepository.create({
         ...productDetails,
-        images: images.map(image => this.productImageRepository.create({ url: image }))
+        images: images.map(image => this.productImageRepository.create({ url: image })),
+        user: user
       })
       await this.productRepository.save(product)
 
@@ -80,7 +82,7 @@ export class ProductsService {
     }
   }
 
-  async update(id: string, updateProductDto: UpdateProductDto) {
+  async update(id: string, updateProductDto: UpdateProductDto, user: User) {
 
     const { images, ...toUpdate } = updateProductDto
 
@@ -103,6 +105,7 @@ export class ProductsService {
         product.images = images.map(image => this.productImageRepository.create({ url: image }))
       }
 
+      product.user = user
       await queryRunner.manager.save(product)
 
       await queryRunner.commitTransaction()
