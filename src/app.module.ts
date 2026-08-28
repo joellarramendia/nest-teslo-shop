c6184c2886bb69ea54d9ebe8477a4f3b42a92ad1
joellarramendia <joellarramendia@gmail.com>
2026-08-27 23:34:33 -0300

    add ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,20 +10,25 @@ import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
 
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+              ? { rejectUnauthorized: false }
+              : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT!,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      password: process.env.DB_PASSWORD,      
       autoLoadEntities: true,
-      synchronize: true
+      synchronize: true,
     }),
 
     ProductsModule,
@@ -33,14 +38,14 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
     SeedModule,
 
     FilesModule,
-    
+
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'..','public')
+      rootPath: join(__dirname, '..', 'public')
     }),
-    
+
     AuthModule,
-    
+
     MessagesWsModule
   ],
 })
-export class AppModule {}
+export class AppModule { }
